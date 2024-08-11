a0024d18d233665787b9dbe37e4e43b19feb0ab6
Tassadar921 <80831213+Tassadar921@users.noreply.github.com>
2024-08-11 14:24:28 +0200

    linter

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,6 +1,6 @@
 const config: { webDir: string; appName: string; appId: string } = {
   appId: 'com.example.mysveltekitapp',
-  appName: 'tassadraft-v2',
+  appName: 'Tassadraft-v2',
   webDir: 'dist',
 };
 
