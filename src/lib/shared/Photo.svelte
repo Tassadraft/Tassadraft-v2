ff6f0c3268646bb88414122e234b5df54b03e94b
Paul Cuisinier <paul.lecuisinier@gmail.com>
2024-11-26 17:24:31 +0100

    improved deck download & photo component

--- src/lib/shared/Photo.svelte
+++ src/lib/shared/Photo.svelte
@@ -2,6 +2,11 @@
     import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
     import Fab from './Fab.svelte';
     import { createEventDispatcher } from 'svelte';
+    import IconButton from "./IconButton.svelte";
+    import Button from "./Button.svelte";
+    import Icon from "./Icon.svelte";
+
+    export let mode = 'fab';
 
     const dispatch = createEventDispatcher();
 
@@ -18,4 +23,13 @@
     };
 </script>
 
+{#if mode === 'fab'}
 <Fab icon="camera" horizontal="middle" vertical="bottom" on:click={takePhoto} />
+{:else if mode === 'inline'}
+    <Button on:click={takePhoto}>
+        <div class="flex gap-1 text-nowrap">
+            <Icon name="camera" />
+            <slot />
+        </div>
+    </Button>
+{/if}
