634b2b3f93d42f1be5c916c9fd26f20ec8ffd2c4
Paul Cuisinier <paul.lecuisinier@gmail.com>
2024-12-03 17:11:28 +0100

    Imp routing

---
+++ src/lib/shared/BackTo.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Icon from "./Icon.svelte";
+    import Button from "./Button.svelte";
+    import Link from "./Link.svelte";
+
+    export let href;
+    export let text;
+</script>
+
+<Button>
+    <Link {href}>
+        <div class="inline-block">
+            <div class="flex flex-row gap-1">
+                <Icon name="arrowLeft" />
+                <p>{text}</p>
+            </div>
+        </div>
+    </Link>
+</Button>
