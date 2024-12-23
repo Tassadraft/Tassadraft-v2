d97814d519edab73f426fea8c908ee1bfe2963c8
Paul Cuisinier <paul.lecuisinier@gmail.com>
2024-12-23 15:21:26 +0100

    Update ConfirmAccountCreation.svelte

--- src/lib/pages/ConfirmAccountCreation.svelte
+++ src/lib/pages/ConfirmAccountCreation.svelte
@@ -5,12 +5,31 @@
     import axios from 'axios';
     import { showToast } from "../../services/toastService.js";
     import { navigate } from '../../stores/locationStore.js';
+    import {updateProfile} from "../../stores/profileStore.js";
 
     export let token = '';
 
     onMount(async () => {
        try {
-           await axios.get(`/api/account-creation/confirm/${token}`);
+           const { data } = await axios.get(`/api/account-creation/confirm/${token}`);
+           localStorage.setItem('apiToken', data.token.token);
+           localStorage.setItem('apiTokenExpiration', data.token.expiresAt);
+           axios.defaults.headers.common['Authorization'] = `Bearer ${data.token.token}`;
+
+           // Profile
+           try {
+               await updateProfile();
+           } catch (e) {
+               showToast($t('toast.profile.get.error'), 'error');
+           }
+
+           // Subscription
+           try {
+               await axios.get('/api/auth/reserved');
+               localStorage.setItem('subscribed', 'true');
+           } catch (error) {
+               localStorage.setItem('subscribed', 'false');
+           }
        } catch (e) {
            showToast($t('toast.create-account.confirm.error'), 'error');
        } finally {
@@ -18,8 +37,6 @@
            navigate('/');
        }
     });
-
-
 </script>
 
 <Title title={$t('create-account.confirm.title')} hasBackground={true} />
