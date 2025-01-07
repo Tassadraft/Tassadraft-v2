3584b026066a1a083ed2dbef7e7a7e7f5c060dd4
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-01-07 22:28:59 +0100

    icon app mobile

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,15 +1,7 @@
-const config: {
-    webDir: string;
-    appName: string;
-    appId: string;
-    loggingBehavior: 'none' | 'debug' | 'production';
-    webContentsDebuggingEnabled: boolean;
-} = {
-    appId: 'com.example.mysveltekitapp',
+export default {
+    appId: 'com.tassadraft.front',
     appName: 'Tassadraft-v2',
     webDir: 'dist',
     loggingBehavior: 'production',
     webContentsDebuggingEnabled: false,
 };
-
-export default config;
